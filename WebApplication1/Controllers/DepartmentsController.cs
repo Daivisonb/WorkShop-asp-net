d25f0ca665017b5c4b5c67678aa0a04d6984c92d
Daivison Batista <daivisonb384@gmail.com>
2019-02-15 21:47:46 -0200

    Adicionamento do Departamento

---
+++ WebApplication1/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronicos" });
+            list.Add(new Department { Id = 2, Name = "Vestimentas" });
+            return View(list);
+        }
+    }
+}
