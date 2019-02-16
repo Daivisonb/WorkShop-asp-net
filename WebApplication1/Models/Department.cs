0f930d119a8a94572c6388502776cc40b10a859d
Daivison Batista <daivisonb384@gmail.com>
2019-02-16 03:50:28 -0200

    Outras Class adicionadas ao banco!

--- WebApplication1/Models/Department.cs
+++ WebApplication1/Models/Department.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
@@ -9,5 +8,22 @@ namespace WebApplication1.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Selles { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Selles.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Selles.Sum(sl => sl.TotalSales(initial, final));
+        }
     }
 }
