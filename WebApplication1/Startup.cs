b1c55c29042b4e8a835bb46c572673e034846215
Daivison Batista <daivisonb384@gmail.com>
2019-02-16 02:46:20 -0200

    Adaptação para o MySql

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -36,8 +36,7 @@ namespace WebApplication1
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<WebApplication1Context>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebApplication1Context")));
+            services.AddDbContext<WebApplication1Context>(options => options.UseMySql(Configuration.GetConnectionString("WebApplication1Context"), builder => builder.MigrationsAssembly("WebApplication1")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
